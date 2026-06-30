7858f143de9aefc52f3a60716074493866a6b0e2
Leif William Sogge <spaceleafcorp@outlook.com>
2026-06-29 23:23:28 -0400

    Add QuantumExecutor class with simulateCircuit method

---
+++ src/QuantumExecutor.ts
@@ -0,0 +1,6 @@
+// src/QuantumExecutor.ts
+export class QuantumExecutor {
+  async simulateCircuit(circuitDescription: string): Promise<any> {
+    // Call external service or local simulator
+  }
+}
